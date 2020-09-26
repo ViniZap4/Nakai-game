c6ce40118ac9f582b7dfb94f7761b8f757023341
Vinicius Zapala dos Santos <vinizap4@outlook.com>
2020-09-26 17:03:03 -0300

    update script naki character

--- props/characters/nakai/scripts/nakai.gd
+++ props/characters/nakai/scripts/nakai.gd
@@ -18,58 +18,65 @@ func _on_Timer_timeout():
 
 func _physics_process(delta):
 	var motion = Vector2()
-	
+
 	if Input.is_action_pressed("roll") and in_roll == false:
 		in_roll = true
 		MOTION_SPEED = 300 
 		$Timer.connect("timeout", self, "_on_Timer_timeout")
 		$Timer.start()
 	
+	
+	if Input.is_action_pressed("move_up"):
+		motion += Vector2(-1, -1)
+				
+	if Input.is_action_pressed("move_down"):
+		motion += Vector2(1, 1)
+
+	if Input.is_action_pressed("move_left"):
+		motion += Vector2(-1, 1)
+		
+	if Input.is_action_pressed("move_right"):
+		motion += Vector2(1, -1)
+
+	
 	if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_left"):
-		motion += Vector2(-2, 0)
 		idle = "idle_wa"
 		roll = "roll_wa"
 		run = "run_wa" 
 		
 		
 	elif Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right"):
-		motion += Vector2(0, -2)
 		idle = "idle_dw"
 		roll = "roll_wd"
 		run = "run_wd"
 		
 	elif Input.is_action_pressed("move_down") and Input.is_action_pressed("move_left"):
-		motion += Vector2(0, 2)
 		idle = "idle_as"
 		roll = "roll_sa"
 		run = "run_as"
 
 	elif Input.is_action_pressed("move_down") and Input.is_action_pressed("move_right"):
-		motion += Vector2(2, 0)
 		idle = "idle_sd"
 		roll = "roll_sd"
 		run = "run_sd"
 		
 	elif Input.is_action_pressed("move_up"):
-		motion += Vector2(-1.2, -1.2)
 		idle = "idle_w"
 		roll = "roll_w"
 		run = "run_w"
 		
 	elif Input.is_action_pressed("move_down"):
-		motion += Vector2(1.2, 1.2)
 		idle = "idle_s"
 		roll = "roll_s"
 		run ="run_s"
 		
 	elif Input.is_action_pressed("move_left"):
-		motion += Vector2(-1.2, 1.2)
 		idle = "idle_a"
 		roll = "roll_a"
 		run = "run_a"
 		
 	elif Input.is_action_pressed("move_right"):
-		motion += Vector2(1.2, -1.2)
+
 		idle = "idle_d"
 		roll = "roll_d"
 		run = "run_d"
