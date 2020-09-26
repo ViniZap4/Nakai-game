0ce7d9f00a8d94676ee3c6954deb7385b4a97e2c
Vinicius Zapala dos Santos <vinizap4@outlook.com>
2020-09-26 17:51:16 -0300

    update nakai character

--- props/characters/nakai/scripts/nakai.gd
+++ props/characters/nakai/scripts/nakai.gd
@@ -2,15 +2,16 @@ extends KinematicBody2D
 
 var MOTION_SPEED = 160 
 
-
-var idle = "idle_as"
-var run = "run_as"
-var roll = "idle_sa"
+var direction = "sd"
+var idle = "idle_sd"
+var run = "run_sd"
+var roll = "idle_sd"
 
 var in_roll = false
+var vel_roll = Vector2(0,0)
 
 func cartesian_to_isometric(cartesian):
-	return Vector2(cartesian.x - cartesian.y, (cartesian.x + cartesian.y)/2 )
+	return Vector2(cartesian.x - cartesian.y, (cartesian.x + cartesian.y)/1.7 )
 
 func _on_Timer_timeout():
 	in_roll = false
@@ -19,68 +20,66 @@ func _on_Timer_timeout():
 func _physics_process(delta):
 	var motion = Vector2()
 
-	if Input.is_action_pressed("roll") and in_roll == false:
-		in_roll = true
-		MOTION_SPEED = 300 
-		$Timer.connect("timeout", self, "_on_Timer_timeout")
-		$Timer.start()
+	
+	if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_left") and in_roll == false:
+		direction = "wa"
+		
+	elif Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right") and in_roll == false:
+		direction = "wd"
+		
+	elif Input.is_action_pressed("move_down") and Input.is_action_pressed("move_left") and in_roll == false:
+		direction = "sa"
+
+	elif Input.is_action_pressed("move_down") and Input.is_action_pressed("move_right") and in_roll == false:
+		direction = "sd"
+		
+	elif Input.is_action_pressed("move_up") and in_roll == false:
+		direction = "w"
+		
+	elif Input.is_action_pressed("move_down") and in_roll == false:
+		direction = "s"
+		
+	elif Input.is_action_pressed("move_left") and in_roll == false:
+		direction = "a"
+		
+	elif Input.is_action_pressed("move_right") and in_roll == false:
+		direction = "d"
+		
+	
+	idle = "idle_" + direction
+	roll = "roll_" + direction
+	run = "run_" + direction
 	
 	
-	if Input.is_action_pressed("move_up"):
+	
+	
+	
+	if Input.is_action_pressed("move_up") and in_roll == false:
 		motion += Vector2(-1, -1)
 				
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("move_down") and in_roll == false:
 		motion += Vector2(1, 1)
 
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("move_left") and in_roll == false:
 		motion += Vector2(-1, 1)
 		
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right") and in_roll == false:
 		motion += Vector2(1, -1)
 
 	
-	if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_left"):
-		idle = "idle_wa"
-		roll = "roll_wa"
-		run = "run_wa" 
-		
-		
-	elif Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right"):
-		idle = "idle_dw"
-		roll = "roll_wd"
-		run = "run_wd"
-		
-	elif Input.is_action_pressed("move_down") and Input.is_action_pressed("move_left"):
-		idle = "idle_as"
-		roll = "roll_sa"
-		run = "run_as"
+	if in_roll == true:
+		motion += vel_roll
 
-	elif Input.is_action_pressed("move_down") and Input.is_action_pressed("move_right"):
-		idle = "idle_sd"
-		roll = "roll_sd"
-		run = "run_sd"
-		
-	elif Input.is_action_pressed("move_up"):
-		idle = "idle_w"
-		roll = "roll_w"
-		run = "run_w"
-		
-	elif Input.is_action_pressed("move_down"):
-		idle = "idle_s"
-		roll = "roll_s"
-		run ="run_s"
-		
-	elif Input.is_action_pressed("move_left"):
-		idle = "idle_a"
-		roll = "roll_a"
-		run = "run_a"
-		
-	elif Input.is_action_pressed("move_right"):
 
-		idle = "idle_d"
-		roll = "roll_d"
-		run = "run_d"
+	if Input.is_action_pressed("roll") and in_roll == false and motion != Vector2(0, 0):
+		in_roll = true
+		MOTION_SPEED = 360 
+		vel_roll = motion
 		
+		$Timer.connect("timeout", self, "_on_Timer_timeout")
+		$Timer.start()
+
+	
 	
 	if motion !=  Vector2(0, 0): 
 		if in_roll == false:
@@ -90,6 +89,7 @@ func _physics_process(delta):
 	else:
 		$AnimatedSprite.play(idle)
 
+	print(vel_roll) 
 
 	motion = motion * MOTION_SPEED
 	motion = cartesian_to_isometric(motion)
