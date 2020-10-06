0bb08bcf73ef3db9d46dcf835821d9766c313678
Lucas Ribeiro <lucas2001.789@gmail.com>
2020-10-05 22:35:29 -0300

    Importando assets e warrior na cena

---
+++ props/characters/warrior/scripts/warrior.gd
@@ -0,0 +1,70 @@
+extends KinematicBody2D
+
+var speed = 100
+var maxHealth = 100
+var enemyDamage = 25
+
+var ai_think_time = 0.7
+var ai_think_time_timer = null
+
+var idle = "idle_sd"
+var run = "run_a"
+var roll = "idle_sd"
+
+var reflexes = 4
+onready var target = $"../nakai"
+var is_in_range = false
+
+var health = 0
+
+func _onready():
+	health = maxHealth;
+
+func _ready():
+	_onready()
+	setup_ai_think_time_timer()
+
+func take_Damage(damageCount):
+	health -= damageCount;
+	
+	if(health <= 0):
+		health = 0
+		$AnimationPlayer.play("die")
+		queue_free()
+		
+		
+func attack():
+	var damage = round(randi() % 50 + enemyDamage / 10)
+	target.take_damage(damage);
+
+func ai_get_direction():
+	return target.position - self.position
+
+func ai_move():
+	var direction = ai_get_direction();
+	var motion = direction.normalized() * speed;
+	move_and_slide(motion)
+	
+func setup_ai_think_time_timer():
+	ai_think_time_timer = Timer.new()
+	ai_think_time_timer.set_one_shot(true)
+	ai_think_time_timer.set_wait_time(ai_think_time)
+	ai_think_time_timer.connect("timeout", self, "on_ai_thinktime_timeout_complete")
+	add_child(ai_think_time_timer)
+
+func decide_to_attack():
+	ai_think_time_timer.start()
+	
+func on_ai_thinktime_timeout_complete():
+	if is_in_range:
+		attack()
+
+func _process(delta):
+	if is_in_range && ai_think_time_timer.time_left == 0:
+		decide_to_attack()
+	else:
+		ai_move()
+		run = "run_a";
+		$AnimatedSprite.play(run)
+	
+
