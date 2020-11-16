fae315d37e3d067cff03089f62cd4e90d04926d5
Vinicius Zapalá dos Santos <vinizap4@outlook.com>
2020-11-16 12:05:35 -0300

    updating scenes

--- scenes/ui/game-over/scripts/Restart.gd
+++ scenes/ui/game-over/scripts/Restart.gd
@@ -18,5 +18,5 @@ func _on_Restart_pressed():
 	$Sprite.modulate.r = 0.1
 	$Sprite.modulate.g = 0.1
 	$Sprite.modulate.b = 0.1
-	get_tree().change_scene("res://Main.tscn")
+	get_tree().change_scene("res://scenes/Kingdoms/TraoriGrindolynGrindohr/TraoriGrindolynGrindohr.tscn")
 	pass # Replace with function body.
