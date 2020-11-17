0c12b55fac693ff7a8198ea935d7983753be9df2
Vinicius Zapalá dos Santos <vinizap4@outlook.com>
2020-11-17 19:58:40 -0300

    skills

---
+++ scenes/ui/hud/props/skillsInterface.gd
@@ -0,0 +1,26 @@
+extends Control
+
+onready var energy_bar = get_parent().get_node("BarStatus/StaminaBar/MarginContainer/Label/TextureProgress")
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	var energyValue = energy_bar.value
+
+	get_node("skill1/skill1").value = setSkillValue(210, energyValue)
+	get_node("skill2/skill2").value = setSkillValue(180, energyValue)
+	get_node("skill3/skill3").value = setSkillValue(350, energyValue)
+	get_node("skill4/skill4").value = setSkillValue(100, energyValue)
+	get_node("skill5/skill5").value = setSkillValue(100, energyValue)
+
+
+func setSkillValue(value, energyValue):
+	
+	var newValue = value - energyValue
+	
+	if newValue < 0:
+		newValue = 0
+	return  newValue
