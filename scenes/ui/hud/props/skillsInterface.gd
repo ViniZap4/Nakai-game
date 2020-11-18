a71a10e9fd3631c38e79f96078e92239ba86dacc
Vinicius Zapalá dos Santos <vinizap4@outlook.com>
2020-11-18 13:40:13 -0300

    updating map skills

--- scenes/ui/hud/props/skillsInterface.gd
+++ scenes/ui/hud/props/skillsInterface.gd
@@ -1,5 +1,8 @@
 extends Control
 
+
+
+
 onready var energy_bar = get_parent().get_node("BarStatus/StaminaBar/MarginContainer/Label/TextureProgress")
 
 
@@ -10,26 +13,33 @@ func _ready():
 func _physics_process(delta):
 	var energyValue = energy_bar.value
 
-	get_node("skill1/skill1").value = setSkillValue(210, energyValue)
-	get_node("skill2/skill2").value = setSkillValue(180, energyValue)
-	get_node("skill3/skill3").value = setSkillValue(350, energyValue)
-	get_node("skill4/skill4").value = setSkillValue(100, energyValue)
-	get_node("skill5/skill5").value = setSkillValue(200, energyValue)
+	get_node(skill(1)).value = setSkillValue(210, energyValue)
+	get_node(skill(2)).value = setSkillValue(180, energyValue)
+	get_node(skill(3)).value = setSkillValue(350, energyValue)
+	get_node(skill(4)).value = setSkillValue(100, energyValue)
+	get_node(skill(5)).value = setSkillValue(200, energyValue)
 	
 	
-	changeModulate("attack_one", "skill1/caption1", Color(0,1,0.93))
-	changeModulate("attack_two", "skill2/caption2", Color(0.96,0.35,0.21))
-	changeModulate("attack_three", "skill3/caption3", Color(0.87,0,0))
-	changeModulate("lifeUp", "skill4/caption4", Color(0,0.87,0.87))
-	changeModulate("roll", "skill5/caption5", Color(0.27,0.99,0.26))
+	changeModulate("attack_one", capt(1), Color(0,1,0.93))
+	changeModulate("attack_two", capt(2), Color(0.96,0.35,0.21))
+	changeModulate("attack_three", capt(3), Color(0.87,0,0))
+	changeModulate("lifeUp", capt(4), Color(0,0.87,0.87))
+	changeModulate("roll", capt(5), Color(0.27,0.99,0.26))
 
+func skill(num):
+	return "skill"+ str(num) +"/skill"+ str(num)
+func capt(num):
+	return "skill"+str(num)+"/caption"+ str(num)
+
+func WhiteModulate(node):
+	get_node(node).modulate = Color(1,1,1)
 
 
 func changeModulate(action, node, color):
 	if Input.is_action_pressed(action):
 		get_node(node).modulate = color
 	if !Input.is_action_pressed(action):
-		get_node(node).modulate = Color(1,1,1)
+		WhiteModulate(node)
 
 func setSkillValue(value, energyValue):
 	
@@ -43,7 +53,6 @@ func setSkillValue(value, energyValue):
 func _on_skill1_pressed():
 	Input.action_press("attack_one")
 
-
 func _on_skill2_pressed():
 	Input.action_press("attack_two")
 	
@@ -55,4 +64,21 @@ func _on_skill4_pressed():
 
 func _on_skill5_pressed():
 	Input.action_press("roll")
+	
 
+
+
+func _on_skill1_mouse_exited():
+	Input.action_release("attack_one")
+	
+func _on_skill2_mouse_exited():
+	Input.action_release("attack_two")
+
+func _on_skill3_mouse_exited():
+	Input.action_release("attack_three")
+
+func _on_skill4_mouse_exited():
+	Input.action_release("lifeUp")
+
+func _on_skill5_mouse_exited():
+	Input.action_release("roll")
